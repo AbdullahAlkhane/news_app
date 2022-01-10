641b22769f2d10b8894442658440923aaa7227ce
ABDULLAH <abdullahalkhane@gmail.com>
2022-01-11 00:28:57 +0200

    Initial commit

---
+++ lib/news_app/sports/sports_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:news_app/news_layout/cubit/cubit_.dart';
+import 'package:news_app/news_layout/cubit/states.dart';
+import 'package:news_app/shared/componants/conbonants.dart';
+
+class SportsScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<NewsCubit, NewsAppStats>(
+        listener:(context,state){},
+        builder: (context, state) {
+          var list = NewsCubit.get(context).sports;
+          return buildArcticle(context,list: list);
+        }
+    );
+  }
+}
